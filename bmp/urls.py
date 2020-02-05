3dfde71bbd134dc8fa544b77e85ba3dc85bfd5a6
Leobod <13952348337@sina.cn>
2020-02-05 22:48:12 +0800

    名称：bmp-首页 描述：提交系统首页 时间：2020-02-05 作者：leobod（leobod@eside.cn） 备注：无

--- bmp/urls.py
+++ bmp/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from bmp import views as IndexViews
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('Account/', include('Account.urls')),
+    url(r'^', IndexViews.index)
 ]
